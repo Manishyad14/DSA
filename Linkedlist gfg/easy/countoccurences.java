c16fd2a6795fb5c022817202543cfab08de21227
Manish Yadav <141780524+Manishyad14@users.noreply.github.com>
2024-01-21 23:28:56 +0530

    Add files via upload

---
+++ Linkedlist gfg/easy/countoccurences.java
@@ -0,0 +1,28 @@
+class Solution
+{
+    public static int count(Node head, int search_for)
+    {
+        Node temp = head;
+        int count  = 0;
+
+        while(temp!=null){
+
+            if(temp.data == search_for){
+                count ++ ;
+
+            }
+
+            temp = temp.next;
+
+
+
+        }
+
+        return count ;
+
+
+
+
+
+    }
+}
