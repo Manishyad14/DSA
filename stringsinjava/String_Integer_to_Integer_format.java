871e31630a37d0dc6495997269f2accc7a7cecdb
Manish Yadav <141780524+Manishyad14@users.noreply.github.com>
2024-09-19 17:18:53 +0530

    Add files via upload

---
+++ stringsinjava/String_Integer_to_Integer_format.java
@@ -0,0 +1,12 @@
+package Veryimp_STRINGSSSS;
+import java.util.*  ;
+
+public class String_Integer_to_Integer_format {
+    public static void main(String[] args) {
+        Scanner sc=  new Scanner(System.in) ;
+        String n = sc.nextLine() ;
+
+        int num = Integer.parseInt(n) ;
+        System.out.println(num);
+    }
+}
